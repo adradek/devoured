c0feebb0d4fe55dc134adc99dea4a897e6b5802d
Alex Kochurov <alex.kochurov@gmail.com>
2017-03-14 20:52:46 +0300

    authentication with sorcery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :require_login
+
+  private
+
+    def not_authenticated
+      redirect_to login_url, alert: "First login to access this page."
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'welcome#welcome'
+
+  get    'login',  to: "sessions#new",       as: 'login'
+  delete 'logout', to: "sessions#destroy",   as: 'logout'
+  get    'signup', to: "users#new",          as: 'signup'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
