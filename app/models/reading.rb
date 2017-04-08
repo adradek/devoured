90047f80c8bed284eec85ad984e9e9ff7150b126
Alex Kochurov <alex.kochurov@gmail.com>
2017-04-08 23:37:31 +0300

    change readings view

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,4 +1,12 @@
 class Reading < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  def reading_days
+    ((finish || Time.zone.today) - start).to_i if start
+  end
+
+  def hint
+    "#{reading_days} дней (с #{start.strftime('%d.%m.%y')})" if start
+  end
 end
