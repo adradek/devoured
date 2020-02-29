031718955eadccee6ec3e3de2f13b0f03bb46878
Alex Kochurov <alex.kochurov@gmail.com>
2020-02-29 20:59:01 +0300

    remove pry from production

--- app/controllers/users/books_controller.rb
+++ app/controllers/users/books_controller.rb
@@ -7,7 +7,7 @@ class Users::BooksController < ApplicationController
   end
 
   def create
-    binding.pry
+    # binding.pry
     book = Book.find_by(book_params.slice(:title, :author)) \
           || Book.create!(book_params.merge(compilation: params[:compilation]))
 
