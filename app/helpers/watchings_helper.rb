1a93dbd0577e954b792405623dcf40448ed2711b
Alex Kochurov <alex.kochurov@gmail.com>
2021-08-15 21:38:04 +0300

    display the tomatoes mark

--- app/helpers/watchings_helper.rb
+++ app/helpers/watchings_helper.rb
@@ -1,6 +1,27 @@
 module WatchingsHelper
   def critics(film)
-    imdb = film.imdb? ? (film.imdb / 10.0).to_s : ''
-    content_tag(:span, imdb, class: 'imdb-value')
+    output = []
+    imdb = film.imdb? ? "#{film.imdb / 10.0} " : ''
+    output << content_tag(:span, imdb, class: 'imdb-value')
+    output << tomatoes(film)
+    output.join.html_safe
+  end
+
+  def tomatoes(film)
+    output = ["("]
+    return '' if film.tomatoes.blank?
+
+    all = film.tomatoes_all
+    output << content_tag(:span, all, class: tomatoclass(all))
+
+    top = film.tomatoes_top
+    output << content_tag(:span, "-#{top}", class: tomatoclass(top)) if top
+    output << ")"
+
+    output.join
+  end
+
+  def tomatoclass(rate)
+    rate > 60 ? 'fresh-tomato' : 'rotten-tomato'
   end
 end
