e6d506d48e9940b5d4955c00c7c3b31de0eb1f40
Alex Kochurov <alex.kochurov@gmail.com>
2021-08-17 15:33:43 +0300

    Run annotate

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,19 +2,23 @@
 #
 # Table name: films
 #
-#  id         :integer          not null, primary key
-#  name_rus   :string
-#  name_eng   :string
-#  seria      :boolean          default(FALSE), not null
-#  imdb       :integer
-#  tomatoes   :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :integer          not null, primary key
+#  name_rus     :string
+#  name_eng     :string
+#  seria        :boolean          default(FALSE), not null
+#  imdb         :integer
+#  tomatoes     :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  tomatoes_top :integer
+#  tomatoes_all :integer
 #
 
 class Film < ApplicationRecord
   has_many :intents, as: :intended, dependent: :destroy
   has_many :watchings
+
+  # before_validation :update
 
   def name
     n = name_rus.present? ? name_rus : name_eng
