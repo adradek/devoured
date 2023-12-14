f25211c966f10369070b684595c39fd7654ed935
Alex Kochurov <alex.kochurov@gmail.com>
2023-12-15 02:04:54 +0300

    regained consciousness %)

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,26 @@
 require_relative 'route_constraints'
 
 Rails.application.routes.draw do
-  scope :devoured do
-    root to: 'users#dashboard', constraints: RouteConstraints::UserRequiredConstraint.new
-    root to: 'sessions#new', as: nil
+  root to: 'users#dashboard', constraints: RouteConstraints::UserRequiredConstraint.new
+  root to: 'sessions#new', as: nil
 
-    get    'login',  to: "sessions#new",       as: 'login'
-    delete 'logout', to: "sessions#destroy",   as: 'logout'
-    get    'signup', to: "users#new",          as: 'signup'
+  get    'login',  to: "sessions#new",       as: 'login'
+  delete 'logout', to: "sessions#destroy",   as: 'logout'
+  get    'signup', to: "users#new",          as: 'signup'
 
-    resources :users, param: :secret_id do
-      get 'dashboard', on: :collection
-      resources :films, module: :users do
-        delete 'destroy_intents', on: :member
-      end
-      resources :books, module: :users do
-        delete 'destroy_intents', on: :member
-      end
+  resources :users, param: :secret_id do
+    get 'dashboard', on: :collection
+    resources :films, module: :users do
+      delete 'destroy_intents', on: :member
     end
-
-    resources :compilation_inclusions, only: [:create]
-
-    resources :sessions, only: [:new, :create, :destroy]
-
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :books, module: :users do
+      delete 'destroy_intents', on: :member
+    end
   end
+
+  resources :compilation_inclusions, only: [:create]
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
