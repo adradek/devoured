dd23b87fddb8a9ce8df8f75acc4fb3c1ebe43b2e
Alex Kochurov <alex.kochurov@gmail.com>
2024-10-28 15:33:20 +0300

    experiments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ end
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :file_store, "#{root}/tmp/my_cache" #:memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
