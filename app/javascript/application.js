d8f094a7e3fca26576f53a362498f11a27e99bac
Alex Kochurov <alex.kochurov@gmail.com>
2026-03-03 19:15:32 +0300

    let Bootstrap 5 be!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,5 @@
-import "@fortawesome/fontawesome-free/css/all"
-
 import "bootstrap"
 
-import "./packs/books"
-import "./packs/modals"
-import "./packs/compilation_collapse"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// import "./packs/books"
+// import "./packs/modals"
+// import "./packs/compilation_collapse"
